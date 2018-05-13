994b9c9dfe7f0ee0f868924f29e641a8a2270676
katycarr <khcarr9@gmail.com>
2018-05-12 21:17:41 -0400

    moved navigator to seperate file

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-// import two screens
-import HomeScreen from './screens/Home'
-import LoginScreen from './screens/Login'
-// import StackNavigator
-import { StackNavigator } from 'react-navigation'
-// import navigator
-// import BaseNavigation from './Navigation'
 
-const App = StackNavigator({
-  Home: {screen: HomeScreen},
-  Login: {screen: LoginScreen}
-});
+// import navigator
+import BaseNavigation from './Navigation'
+
+const App = () => (
+  <BaseNavigation />
+)
 
 export default App
